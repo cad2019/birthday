6fa6dcaeb4f34365e0c86faf3c1e0013b0346fc2
cad2019 <43227644+cad2019@users.noreply.github.com>
2020-04-24 16:34:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "横幅",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
