2cb253f4c0b3c35732da866da5c7313a71ee93d8
cad2019 <43227644+cad2019@users.noreply.github.com>
2020-04-24 16:16:09 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ var config = {
         "sing shen fai lo (生辰快乐）！",
         "생일 축하합니다！",
         "お诞生日おめでとうございます！",
-        "С днем рождения!",
-        "Alles Gute Zum Geburtstag!",
-        "iFeliz cumpleaños!",
-        "Buon Compleanno!",
+        "С днем рождения！",
+        "Alles Gute Zum Geburtstag！",
+        "iFeliz cumpleaños！",
+        "Buon Compleanno！",
         "Hongera！",
         "Fortuna dies natalis！",
         "Grattis pa fodelsedagen！",
@@ -29,17 +29,17 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "hi！李依婷小盆友": "./imgs/xiaokeai.jpg",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "hi！李依婷小盆友": "./imgs/16.jpg",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "祝你": "./imgs/15.jpg",
         "岁岁年年 万喜万般宜": "./imgs/1.jpg",
         "祝你": "./imgs/2.jpg",
         "sing shen fai lo (生辰快乐）！": "./imgs/3.jpg",
         "생일 축하합니다！": "./imgs/4.jpg",
         "お诞生日おめでとうございます！": "./imgs/5.jpg",
-        "С днем рождения!": "./imgs/6.jpg",
-        "Alles Gute Zum Geburtstag!": "./imgs/7.jpg",
-        "iFeliz cumpleaños!": "./imgs/8.jpg",
-        "Buon Compleanno!": "./imgs/9.jpg",
+        "С днем рождения！": "./imgs/6.jpg",
+        "Alles Gute Zum Geburtstag！": "./imgs/7.jpg",
+        "iFeliz cumpleaños！": "./imgs/8.jpg",
+        "Buon Compleanno！": "./imgs/9.jpg",
         "Hongera！": "./imgs/10.jpg",
         "Fortuna dies natalis！": "./imgs/11.jpg",
         "Grattis pa fodelsedagen！": "./imgs/12.jpg",
