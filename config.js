cf0673b14c88b25dacb544499be297bfcaf59d10
cad2019 <43227644+cad2019@users.noreply.github.com>
2020-04-24 16:01:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,21 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "hi",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "李依婷小盆友",
-        "生日快乐呀",
+        "hi！李依婷小盆友",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "祝你",
-        "岁岁年年",
-        "万喜万般宜",
-        "旦逢良辰",
-        "顺颂时宜",
+        "岁岁年年 万喜万般宜",
+        "祝你",
+        "sing shen fai lo (生辰快乐）！",
+        "생일 축하합니다！",
+        "お诞生日おめでとうございます！",
+        "С днем рождения!",
+        "Alles Gute Zum Geburtstag!",
+        "iFeliz cumpleaños!",
+        "Buon Compleanno!",
+        "Hongera！",
+        "Fortuna dies natalis！",
+        "Grattis pa fodelsedagen！",
+        "Happy birthday！",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -22,14 +29,22 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "hi": "./imgs/xiaokeai.jpg",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "李依婷小盆友": "./imgs/1.jpg",
-        "生日快乐呀": "./imgs/2.jpg",
-        "祝你": "./imgs/3.jpg",
-        "岁岁年年": "./imgs/4.jpg",
-        "万喜万般宜": "./imgs/5.jpg",
-        "旦逢良辰": "./imgs/6.jpg",
-        "顺颂时宜": "./imgs/7.jpg",
+        "hi！李依婷小盆友": "./imgs/xiaokeai.jpg",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "祝你",: "./imgs/15.jpg",
+        "岁岁年年 万喜万般宜",: "./imgs/1.jpg",
+        "祝你",: "./imgs/2.jpg",
+        "sing shen fai lo (生辰快乐）！",: "./imgs/3.jpg",
+        "생일 축하합니다！",: "./imgs/4.jpg",
+        "お诞生日おめでとうございます！",: "./imgs/5.jpg",
+        "С днем рождения!",: "./imgs/6.jpg",
+        "Alles Gute Zum Geburtstag!",: "./imgs/7.jpg",
+        "iFeliz cumpleaños!",: "./imgs/8.jpg",
+        "Buon Compleanno!",: "./imgs/9.jpg",
+        "Hongera！",: "./imgs/10.jpg",
+        "Fortuna dies natalis！",: "./imgs/11.jpg",
+        "Grattis pa fodelsedagen！",: "./imgs/12.jpg",
+        "Happy birthday！",: "./imgs/13.jpg",
+        "没有啦！": "./imgs/7.jpg",: "./imgs/14.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
